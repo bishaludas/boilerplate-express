4e4f2959624fb3bf5541351826bf994cafade305
bishaludas <supertramp.bishal@gmail.com>
2021-06-19 17:16:50 +0545

    load html file

--- myApp.js
+++ myApp.js
@@ -2,12 +2,10 @@ var express = require('express');
 var app = express();
 //console.log("Hello World");
 
-
+let absolutePath = __dirname + '/views/index.html';
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  res.sendFile(absolutePath)
 })
-
-
 
 
 
