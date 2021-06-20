c1d988d49f0ff3703b783f3f2e4d149a8a4620b5
bishaludas <supertramp.bishal@gmail.com>
2021-06-20 16:15:00 +0545

    add middleware with handler

--- myApp.js
+++ myApp.js
@@ -22,4 +22,11 @@ app.get("/json", function (req, res) {
   res.json({ message: response });
 });
 
+app.get("/now", function(req,res,next){
+  req.time= new Date().toString();
+  next();
+}, function(req,res){
+  res.json({time: req.time});
+})
+
 module.exports = app;
